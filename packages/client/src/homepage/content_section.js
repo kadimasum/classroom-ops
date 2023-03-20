00bca81b308a5ac2d6f615f2f9876760942c9339
kadimasum <kadimasam@gmail.com>
2023-03-20 16:59:07 +0300

    Add content page to home page

--- packages/client/src/homepage/content_section.js
+++ packages/client/src/homepage/content_section.js
@@ -3,8 +3,20 @@ import React from "react";
 
 export default function ContentSection(){
     return (
-        <div>
-            
+        <div className="content_section_homepage">
+            <div className="content_container_homepage">
+                <div className="content_container_left">
+                    <div className="content_left_card">
+                        <div className="content_text_btn_wrapper">
+                            <h1>Manage Operations For Students In Your Institution</h1>
+                            <p>We enable schools maintain consistency in tracking their students’ data between different teams in the company while ensuring that students have the best experience</p>
+                            <button className="get_started_btn">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="content_container_right">
+                </div>
+            </div>
         </div>
     )
 }
